c4a0b04b49e05873f6a8bb6eb1447a24ef440994
Mark J. Olah <markjolah@users.noreply.github.com>
2019-01-16 16:44:42 -0700

    Working with new add_shared_static_libraries()

--- build.debug.sh
+++ build.debug.sh
@@ -1,7 +1,7 @@
 ##!/bin/bash
 
 INSTALL_PATH=_install
-BUILD_PATH=_build
+BUILD_PATH=_build/Debug
 NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
 ARGS=""
 ARGS="${ARGS} -DBUILD_TESTING=On"
@@ -9,4 +9,4 @@ ARGS="${ARGS} -DBUILD_TESTING=On"
 set -ex
 rm -rf $INSTALL_PATH $BUILD_PATH
 cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH ${ARGS}
-VERBOSE=1 cmake --build $BUILD_PATH/Debug --target install -- -j${NUM_PROCS}
+VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j${NUM_PROCS}
