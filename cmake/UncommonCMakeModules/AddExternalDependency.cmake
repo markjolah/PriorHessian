7784941615c4ed0eec8fff735b9962566063deda
Mark J. Olah <markjolah@users.noreply.github.com>
2019-01-16 16:36:23 -0700

    git subrepo pull (merge) cmake/UncommonCMakeModules
    subrepo:
      subdir:   "cmake/UncommonCMakeModules"
      merged:   "bef4482"
    upstream:
      origin:   "https://github.com/markjolah/UncommonCMakeModules.git"
      branch:   "master"
      commit:   "bef4482"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "74339e8"

--- cmake/UncommonCMakeModules/AddExternalDependency.cmake
+++ cmake/UncommonCMakeModules/AddExternalDependency.cmake
@@ -15,8 +15,9 @@
 #
 # useage: AddExternalDependency(<package-name> <package-git-clone-url> [SHARED] [STATIC])
 # Options:
-# SHARED - Require shared libraries.  Build them if not found. [default=ON]
-# STATIC - Require static libraries.  Build them if not found. [default=OFF]
+# STATIC - Require static libraries.  [default=OFF]
+# SHARED - Require shared libraries.  If neither SHARED nor STATIC is set, then SHARED is default.
+# TESTING - Attempt to build testing functionality. [default=OFF]
 # Single-Value arguments:
 # NAME - [required] Name of PROJECT_NAME of the external cmake project
 # URL - URL of git repository or local path to git repository (can be overwritten with ${PROJECT_NAME}URL environment variable giving alternate URL
@@ -25,12 +26,14 @@
 # CMAKELISTS_TEMPLATE - [optional] Template file for the CMakeLists.txt to the building and installing via ExternalProject_Add [default: Templates/External.CMakeLists.txt.in]
 # BUILD_TYPE_COMPATABILITY - [optional] Default: Exact  Options: Exact, Any
 # TOOLCHAIN_FILE - [optional] [Only used if CMAKE_CROSSCOMPILING is true.  Uses CMAKE_TOOLCHAIN_FILE as the default.]
+#Mulit-arguments
+#  PASS_CACHE_VARIABLES - [optional] List of cache variables to pass-through
 set(AddExternalDependency_include_path ${CMAKE_CURRENT_LIST_DIR} CACHE INTERNAL "Path of AddExternalDependency.cmake")
 
 macro(add_external_dependency)
     set(oneArgOpts NAME URL VERSION INSTALL_PREFIX CMAKELISTS_TEMPLATE
                BUILD_TYPE_COMPATABILITY TOOLCHAIN_FILE)
-    cmake_parse_arguments(_ExtProject "SHARED;STATIC" "${oneArgOpts}" "" ${ARGN})
+    cmake_parse_arguments(_ExtProject "SHARED;STATIC;TESTING" "${oneArgOpts}" "PASS_CACHE_VARIABLES" ${ARGN})
     if(NOT _ExtProject_NAME)
         message(FATAL_ERROR "No package name given")
     endif()
@@ -54,6 +57,35 @@ macro(add_external_dependency)
 
     if(CMAKE_CROSSCOMPILING AND NOT _ExtProject_TOOLCHAIN_FILE)
         set(_ExtProject_TOOLCHAIN_FILE ${CMAKE_TOOLCHAIN_FILE})
+    endif()
+    #Determine extra variables to pass on command line.
+    #Try not to pass anything a child would not accept as it generates ugly warnings
+    if(_ExtProject_SHARED)
+        list(APPEND _ExtProject_PASS_CACHE_VARIABLES BUILD_SHARED)
+    endif()
+    if(_ExtProject_STATIC)
+        list(APPEND _ExtProject_PASS_CACHE_VARIABLES BUILD_STATIC)
+    endif()
+    if(_ExtProject_TESTING)
+        list(APPEND _ExtProject_PASS_CACHE_VARIABLES BUILD_TESTING)
+    endif()
+
+    #Fixup each passed cache variable
+    if(_ExtProject_PASS_CACHE_VARIABLES)
+        set(_pass_vars)
+        foreach(_var IN LISTS _ExtProject_PASS_CACHE_VARIABLES)
+            if(${_var} MATCHES "([A-Za-z0-9_]+)=([A-Za-z0-9_]+)")
+                set(_var ${CMAKE_MATCH_1})
+                set(_val ${CMAKE_MATCH_2})
+                if(DEFINED ${_var})
+                    list(APPEND _pass_vars "-D${_var}=${_val}") # Use defined value
+                endif()
+            elseif(DEFINED ${_var})
+                list(APPEND _pass_vars "-D${_var}=${${_var}}") # Use current value
+            endif()
+        endforeach()
+        set(_ExtProject_PASS_CACHE_VARIABLES ${_pass_vars})
+        unset(_pass_vars)
     endif()
 
     #Build shared libraries by default if neither SHARED or STATIC are set
