3a1f77a996a9d3e07bd64e1a6d7bce30e14bed50
Mark J. Olah <markjolah@users.noreply.github.com>
2019-01-16 16:35:14 -0700

    up

--- build.debug.sh
+++ build.debug.sh
@@ -3,10 +3,10 @@
 INSTALL_PATH=_install
 BUILD_PATH=_build
 NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
-COMMON_ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
-rm -rf $INSTALL_PATH/Debug
+ARGS=""
+ARGS="${ARGS} -DBUILD_TESTING=On"
 
-set -e
-
-cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug ${COMMON_ARGS} -DBUILD_TESTING
+set -ex
+rm -rf $INSTALL_PATH $BUILD_PATH
+cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH ${ARGS}
 VERBOSE=1 cmake --build $BUILD_PATH/Debug --target install -- -j${NUM_PROCS}
--- build.test.sh
+++ build.test.sh
@@ -3,12 +3,11 @@
 INSTALL_PATH=_install
 BUILD_PATH=_build/Debug
 NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
-COMMON_ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
-# rm -rf $INSTALL_PATH $BUILD_PATH
+ARGS=""
+ARGS="${ARGS} -DBUILD_TESTING=On"
 
-set -e
-if [ -ne $BUILD_PATH ]; then
-    cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug ${COMMON_ARGS}
-fi
+set -ex
+rm -rf $INSTALL_PATH $BUILD_PATH
+cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH ${ARGS}
 cmake --build $BUILD_PATH/Debug --target install -- -j${NUM_PROCS}
 ctest ${PWD} $BUILD_PATH/Debug -V -j${NUM_PROCS}
