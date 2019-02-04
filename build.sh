1345e288d310a09f17ada51fb791feaa2717e7b8
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-04 14:18:01 -0700

    build system updates

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
-##!/bin/bash
+#!/bin/bash
+# build.sh
+#
+# Simple release build
 
 INSTALL_PATH=_install
 BUILD_PATH=_build
 NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
-COMMON_ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DBUILD_TESTING=ON"
-rm -rf $INSTALL_PATH $BUILD_PATH
 
-set -e
+ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
+ARGS="${ARGS} -DBUILD_STATIC_LIBS=ON"
+ARGS="${ARGS} -DBUILD_SHARED_LIBS=ON"
+ARGS="${ARGS} -DBUILD_TESTING=On"
+ARGS="${ARGS} -DOPT_INSTALL_TESTING=On"
+ARGS="${ARGS} -DOPT_EXPORT_BUILD_TREE=Off"
+ARGS="${ARGS} -DOPT_ARMADILLO_INT64=On"
 
-cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug ${COMMON_ARGS}
-cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=1 ${COMMON_ARGS}
+set -ex
+rm -rf $INSTALL_PATH $BUILD_PATH/Debug $BUILD_PATH/Release
+cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS}
 cmake --build $BUILD_PATH/Debug --target install -- -j${NUM_PROCS}
+cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release ${ARGS}
 cmake --build $BUILD_PATH/Release --target install -- -j${NUM_PROCS}
