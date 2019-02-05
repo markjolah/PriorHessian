1b6ef9fd927e890337ac1715e1119d4efa8d5948
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-04 19:53:13 -0700

    SmarterPackageVersion fixes

--- build.debug.sh
+++ build.debug.sh
@@ -18,4 +18,4 @@ ARGS="${ARGS} -DOPT_ARMADILLO_INT64=On"
 set -ex
 rm -rf $BUILD_PATH
 cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS} $@
-VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j${NUM_PROCS}
+VERBOSE=1 cmake --build $BUILD_PATH --target all -- -j${NUM_PROCS}
