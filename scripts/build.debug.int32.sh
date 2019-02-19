4a3d8b56b16fd8a15a390af82e1b207f958846f1
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-19 13:41:36 -0700

    fix INT32 script

--- scripts/build.debug.int32.sh
+++ scripts/build.debug.int32.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# build.debug.sh <cmake args ...>
-# Debug-only build to local install prefix with build-tree export
-
-INSTALL_PATH=_install
-BUILD_PATH=_build/Debug
+# scripts/build.debug.int32.sh <cmake args ...>
+# Debug-only build to local install prefix with build-tree export and OPT_ARMADILLO_INT64=On
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SRC_PATH=${SCRIPT_DIR}/..
+INSTALL_PATH=${SRC_PATH}/_install
+BUILD_PATH=${SRC_PATH}/_build/Int32.Debug
 NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
 
 ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
@@ -17,5 +18,5 @@ ARGS="${ARGS} -DOPT_ARMADILLO_INT64=Off"
 
 set -ex
 rm -rf $BUILD_PATH
-cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS} $@
+cmake -H${SRC_PATH} -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS} $@
 VERBOSE=1 cmake --build $BUILD_PATH --target all -- -j${NUM_PROCS}
