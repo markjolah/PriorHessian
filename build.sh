164f12742dbb49486669812edf2b4a46fdbd0760
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-26 09:17:38 -0700

    build script update

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# build.sh
+# build.sh <cmake-args...>
 #
-# Release and Debug build to local install prefix with build-tree export
+# Tracker default build script.
 #
-# For safety only delete the _install if and only if INSTALL_PATH hasn't been modified.
+# Clean release-only build to local install prefix with build-tree export support.
+# Cleans up build and install directories.  For safety, deletes the install dir
+# if and only if INSTALL_PATH hasn't been modified from the default "_install"
+
 
 INSTALL_PATH=_install
 BUILD_PATH=_build
@@ -15,16 +18,15 @@ ARGS="${ARGS} -DBUILD_SHARED_LIBS=ON"
 ARGS="${ARGS} -DBUILD_TESTING=On"
 ARGS="${ARGS} -DOPT_INSTALL_TESTING=On"
 ARGS="${ARGS} -DOPT_EXPORT_BUILD_TREE=On"
-ARGS="${ARGS} -DOPT_BLAS_INT64=On"
+ARGS="${ARGS} -DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=Off"
+ARGS="${ARGS} -DOPT_BLAS_INT64=ON"
 
 set -ex
 
 if [ "$INSTALL_PATH" == "_install" ]; then
     rm -rf _install
 fi
-rm -rf $BUILD_PATH/Debug
+
 rm -rf $BUILD_PATH/Release
-cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS}
-cmake --build $BUILD_PATH/Debug --target install -- -j${NUM_PROCS}
 cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release ${ARGS}
 cmake --build $BUILD_PATH/Release --target install -- -j${NUM_PROCS}
